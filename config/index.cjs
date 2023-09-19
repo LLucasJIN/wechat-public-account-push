688d2e044e7a27e7877da52446698617769e0d54
LLucasJIN <145369455+LLucasJIN@users.noreply.github.com>
2023-09-19 23:30:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2iug6ZZlpvwfJy6uxowlsLHYX0g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'kegTH4gFlfavQ7WNMNmQ_GaC4e_QHirzvxxktLNJ0j0',
+      useTemplateId: 'GB2adoguvo09NUYpTMYHyXbHWc7c48PV3gEYPJT_V-A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-26',
       festivals: [
