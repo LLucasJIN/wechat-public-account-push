3b14dc7e234d4880445d625a7da883dd9abba4c3
LLucasJIN <145369455+LLucasJIN@users.noreply.github.com>
2023-09-24 19:47:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2iug6ZZlpvwfJy6uxowlsLHYX0g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2TJnod-M0y-wkumE4Xn3SKz0BmUKc-EgxtUjI4PUfcA',
+      useTemplateId: '',
       province: '内蒙古',
       city: '呼和浩特',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -50,7 +50,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       id: 'o2iug6Sx0IQXXCPhgPDDXJZrwSsc',
-      useTemplateId: '2TJnod-M0y-wkumE4Xn3SKz0BmUKc-EgxtUjI4PUfcA',
+      useTemplateId: 'bR-Yw8nDWfN8DIYKKkFQ-8LLhMowXGqH5m3pUy7I3ek',
       province: '广东',
       city: '广州',
       horoscopeDate: '10-10',
