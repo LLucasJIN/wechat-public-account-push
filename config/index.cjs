4e026d4ca0884e5f1dc5375384b1b888d8403346
LLucasJIN <145369455+LLucasJIN@users.noreply.github.com>
2023-09-24 20:03:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -50,7 +50,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       id: 'o2iug6Sx0IQXXCPhgPDDXJZrwSsc',
-      useTemplateId: 'TQxd354KKMVtIhGdbMI30nU0CS6raN_lnzcpGKeCpwQ',
+      useTemplateId: 'd5qreTgUNnNwmEFhvWOJhJJhReQT4KaRYDbeKWCsoeY',
       province: '广东',
       city: '广州',
       horoscopeDate: '10-10',
